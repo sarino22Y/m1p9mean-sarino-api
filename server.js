d1f95eb31a78dd0ece5d6a8ba411e43d6eb040e0
Sarino MALALANIRINA <sarino.malalanirna@esti.mg>
2022-10-25 10:55:55 +0300

    Connect mongoDb Atlas

--- server.js
+++ server.js
@@ -15,15 +15,22 @@ const passport = require("passport");
 const app = express();
 const port = process.env.PORT || 3000;
 // const connectionString = "mongodb://localhost:27017/ekaly22y";
-const connectionString = "mongodb+srv://sarino:sarino@cluster0.xzdv9.mongodb.net/ekaly22y?retryWrites=true&w=majority";
+// const connectionString = "mongodb+srv://sarino:Sarino22Y@cluster0.xzdv9.mongodb.net/ekaly22y?retryWrites=true&w=majority";
+const connectionString = "mongodb+srv://sarino:Sarino22Y@cluster0.pm0o0gg.mongodb.net/ekaly22y?retryWrites=true&w=majority";
 
-app.use((req, res, next) =>{
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,POST,PUT,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-})
+// app.use((req, res, next) =>{
+//     res.header("Access-Control-Allow-Origin", req.headers.origin);
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+//     res.header("Access-Control-Allow-Methods", "GET,HEAD,POST,PUT,DELETE,OPTIONS");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     next();
+// });
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
